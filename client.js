78c052ebcb9d26d19005977d95a60246bf4f9e6d
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-05-17 17:08:38 -0500

    comments added for clarification

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ console.log('Here are all the available people:', people);
 $(document).ready( readyNow );
 
 
-// Declaring randomIndex 
+// Declaring randomIndex so I can access it using the people array
 let randomIndex = randomNumber(0, (people.length - 1));
 
 function readyNow() {
@@ -16,6 +16,8 @@ function readyNow() {
     </div>
     `)
 
+    // For loop cycles through the people array to append each profile picture to the DOM
+    // based off their githubUsername
     for( let person of people ) {
         $( '.pictures' ).append(
             `<div data-name=${person.name} class="profilePic">
@@ -30,17 +32,23 @@ function readyNow() {
 
 }
 
+// A generic randomNumber function
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
+// Create a function to generate a randomPerson from the people array using the randomIndex variable 
+// declared up top
 function randomPerson() {
     return people[randomIndex].name
 }
 
+// Create a function to guess who
 function guessWho() {
     console.log('guess who!');
     
+    // If the data variable "name" (found on the data-name on the div of each image) is equal
+    // to the randomPerson that was generated, either success or try again
     if( $(this).data( 'name' ) === randomPerson()) {
         alert("Success! You got it right!");
     } else {
